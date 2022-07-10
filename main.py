2a8b4e5c0e06a92fa6577e3090158c726f62e8e5
monjara <yaginumee@gmail.com>
2022-07-10 21:58:41 +0900

    optional params

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from enum import Enum
 from fastapi import FastAPI
 
@@ -10,16 +11,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {'item_id': item_id}
-
-@app.get('/model/{model_name}')
-async def read_model(model_name: ModelName):
-    if model_name == ModelName.alex:
-        return {'model_name': model_name}
-
-@app.get('/file/{file_path:path}')
-async def read_fiel(file_path: str):
-    return {'file_path': file_path}
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
