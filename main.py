e9dad271c0f02043fcb1cb39354562b60be08343
monjara <yaginumee@gmail.com>
2022-07-10 21:52:56 +0900

    path params

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alex = 'alex'
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+@app.get('/model/{model_name}')
+async def read_model(model_name: ModelName):
+    if model_name == ModelName.alex:
+        return {'model_name': model_name}
+
+@app.get('/file/{file_path:path}')
+async def read_fiel(file_path: str):
+    return {'file_path': file_path}
 
