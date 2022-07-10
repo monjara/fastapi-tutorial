4925eaab869d7222f5b4e39948f5e7685b37f041
monjara <yaginumee@gmail.com>
2022-07-10 22:02:33 +0900

    query params

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None):
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
